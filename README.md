c5936142be8fef65cb8d329d2fc91c0dd8a47a56
Henrique Cesar <hcesarjs@gmail.com>
2019-03-05 23:03:28 -0300

    Bugs corrigidos, melhorias no layout, adicionado logo no PDF

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # avaliacao-even3
 
+## Descrição
+
+Este projeto foi construído para seletiva de estagiários da [Even3](https://even3.com.br).
+
+Foi solicitado construir uma página onde:
+
+1. O usuário pudesse montar o layout de um crachá.
+2. Inserir tags no crachá para que fossem posteriormente impressas.
+3. Copiar o conteúdo de uma planilha do excel e colar em uma caixa de texto.
+4. Fazer o download dos crachás.
+
 ## Como funciona
 
-O usuário precisará fazer o download de todos os arquivos e executar o index.html.
+* O usuário precisará fazer o download de todos os arquivos e executar o index.html.
+* Ao inserir a tabela no campo de texto o usuário deverá inserir o cabeçalho contendo as tags na primeira linha e adicionar "{{ }}" em todas elas, independente de terem sido inseridas no crachá.
+
+Obs: É recomendável inserir um fundo com a resolução 1240 x 1748 (A6) ou proporcional.
 
 ## Informações sobre o projeto
+
+### Ferramentas utilizadas
+
+* JavaScript
+* jQuery v3.3.1
+* Bootstrap v4.1.3
+* JsPDF
 
 ### Requisitos
 
@@ -15,9 +36,16 @@ O usuário precisará fazer o download de todos os arquivos e executar o index.h
 - [x] Validar tags no texto.
 - [x] Gerar arquivo PDF com os crachás dos participantes.
 
-### Ferramentas utilizadas
+### Add-on
 
-* JavaScript
-* Bootstrap
-* jQuery 3.3.1
-* JsPDF
+- [x] O usuário pode inserir a logo do evento.
+- [x] Permite redimensionamento da logo no crachá.
+- [x] PDF gerado com o tamanho da folha A6.
+
+### Limitações
+
+* Não realiza o "crop" do fundo do crachá. (Necessário para fundos desproporcionais ao A6).
+* Imprime apenas texto alinhado à esquerda.
+* Não imprime sublinhamento do texto.
+* Textos não responsivos (textos serão cortados se ultrapassarem a margem do arquivo PDF).
+
--- _js/version1.js
+++ _js/version1.js
@@ -2,21 +2,10 @@
 
 var $tagsNoCracha = [];
 var $tagsNaPlanilha = [];
-
+var $logoInserida = false;
+var tagsCount = document.getElementById("numTags");
 
 /*-------*/
-
-
-function tagsPlanilhas(texto){
-  var cabecalho = texto.slice(0, texto.indexOf("\n"));
-  var regex = new RegExp(/{{(.*?)}}/, "g")
-  var tags = [];
-  var match;
-  while ((match = regex.exec(cabecalho))){
-    tags.push(match[1].toLowerCase());
-  };
-  return tags;
-};
 
 function criarParticipantes(texto){
   var tagsNaPlanilha = getTagsNaPlanilha(texto);
@@ -40,97 +29,87 @@ function criarParticipantes(texto){
 };
 
 $("#texto-planilha").on('keyup', function(){
-  verificarTags(tagsNoCracha, getTagsNaPlanilha($("#texto-planilha").val()));
+  getTagsNaPlanilha($(this).val());
+  verificarTags();
 });
-
-var tagsCount = document.getElementById("numTags");
-var textArea = document.getElementById("texto-planilha");
 
 $("#baixar").on("click", function(){
   var participantes = criarParticipantes();
   if (participantes){
-    var doc = new jsPDF('p', 'mm', 'a6'),
-      linhaY = 20;
-      cracha = document.getElementsByClassName("fundo-cracha")[0].src;
-      for (let key = 0; participantes[key]; key++){
-        doc.addImage(cracha, 'JPEG', 0, 0, 105, 148);
-        for (let campo = 0; $tagsNoCracha[campo]; campo++){
-          /*Salvar estilo*/
-          /*Tamanho*/
-          var target = document.getElementById("edit-text-"+$tagsNoCracha[campo]);
-          doc.setFontSize(((target.style.fontSize).slice(0, -2)));
-          /*Estilo da Fonte*/
-          if (target.style.fontWeight === "bold" && target.style.fontStyle === "italic"){
-            doc.setFontType('bolditalic');
-          } else if (target.style.fontWeight === "bold") {
-            doc.setFontType('bold');
-          } else if (target.style.fontStyle === "italic") {
-            doc.setFontType('italic');
-          } else {
-            doc.setFontType('normal');
-          };
-          /*Cor da fonte*/
-          switch (target.style.color) {
-            case "white":
-              doc.setTextColor(255, 255, 255);
-              break;
-            case "blue":
-              doc.setTextColor(19, 133, 247);
-              break;
-            case "orange":
-              doc.setTextColor(229, 105, 22);
-              break;
-            case "pink":
-              doc.setTextColor(229, 22, 184);
-              break;
-            case "green":
-              doc.setTextColor(33, 196, 36);
-              break;
-            case "red":
-              doc.setTextColor(196, 33, 33);
-              break;
-            default:
-              doc.setTextColor(0, 0, 0);
-          };
-          var areaPos = document.getElementById("area-edit-"+$tagsNoCracha[campo]);
-          let y = (areaPos.style.top).slice(0,-2);
-          var x = (areaPos.style.left).slice(0,-2);
-          doc.text((participantes[key][$tagsNoCracha[campo]]), (x * 0.22), (y * 0.22));
-          linhaY += 10;
-        };
-        doc.addPage();
-      };
-
-    var pageCount = doc.internal.getNumberOfPages();
-    doc.deletePage(pageCount);
-    doc.save();
+    gerarPDF(participantes);
   };
 });
 
-textArea.addEventListener('keyup', function(){
-  verificarTags(tagsNoCracha, getTagsNaPlanilha(textArea.value));
-});
-
-/*POS REFACTOR*/
-
-function addTagNoCracha(tag){
-  if (tag){
-    tag = tag.toLowerCase();
-    if ($tagsNoCracha.indexOf(tag) === -1){
-      $tagsNoCracha.push(tag);
-      return true;
+function gerarPDF(participantes){
+  var doc = new jsPDF('p', 'mm', 'a6'),
+    cracha = document.getElementsByClassName("fundo-cracha")[0].src,
+    fatorEscala = 2.851351351351351, /*Valor que converte o tamanho em 'px' para 'mm' no crachá*/
+    margemDoTopo = 172.5, /*Posição Y absoluta do inicio do crachá*/
+    margemDaEsquerda = 72, /*Posição X absoluta*/
+    escalaFonte = 4, /*Aumenta a escala da fonte para o arquivo PDF*/
+    adicionalEixoY = 5.6; /*Margem Y do jsPDF para arquivos em A6*/
+    if ($logoInserida){
+      var elementoLogo = document.getElementById("logo-img-edit"),
+        logo = document.getElementById("logo-img-edit").src,
+        areaLogo = document.getElementById("logo-edit"),
+        logoHeight = parseFloat((areaLogo.style.height).slice(0, -2)),
+        logoWidth = parseFloat((areaLogo.style.width).slice(0, -2));
     };
-  };
-  return false;
+    for (let key = 0; participantes[key]; key++){
+      doc.addImage(cracha, 'JPEG', 0, 0, 105, 148);
+      for (let campo = 0; $tagsNoCracha[campo]; campo++){
+        var texto = document.getElementById("edit-text-"+$tagsNoCracha[campo]);
+        setPropriedadesForTextPDF(doc, texto, escalaFonte);
+        var area = document.getElementById("area-edit-"+$tagsNoCracha[campo]);
+        let position = setAreaPositionEscalar(area, margemDoTopo, margemDaEsquerda, fatorEscala, adicionalEixoY);
+        doc.text((participantes[key][$tagsNoCracha[campo]]), position[0], position[1]);
+      };
+      if ($logoInserida){
+        let area = document.getElementById("logo-edit");
+        let position = setAreaPositionEscalar(area, margemDoTopo, margemDaEsquerda,
+           fatorEscala, 0);
+        doc.addImage(logo, 'PNG', position[0], position[1],
+        (logoWidth/fatorEscala), (logoHeight/fatorEscala));
+      };
+      doc.addPage();
+    };
+  var pageCount = doc.internal.getNumberOfPages();
+  doc.deletePage(pageCount);
+  doc.save();
 };
 
-function removeTagDoCracha(tag){
-  if ($tagsNoCracha.indexOf(tag) != -1){
-    $tagsNoCracha.pop(tag);
-    return true;
+function setPropriedadesForTextPDF(docpdf, textElement, addFonteSize){
+  /*Tamanho*/
+  let tamanhoFonte = parseFloat((textElement.style.fontSize).slice(0, -2)) + addFonteSize;
+  docpdf.setFontSize(tamanhoFonte);
+  /*Estilo*/
+  if (textElement.style.fontWeight === "bold" && textElement.style.fontStyle === "italic"){
+    docpdf.setFontType('bolditalic');
+  } else if (textElement.style.fontWeight === "bold") {
+    docpdf.setFontType('bold');
+  } else if (textElement.style.fontStyle === "italic") {
+    docpdf.setFontType('italic');
+  } else {
+    docpdf.setFontType('normal');
   };
-  return false;
-}
+  /*Cor*/
+  let rgb = textElement.style.color;
+  rgb = rgb.slice(4, rgb.length-1).split(", ");
+  docpdf.setTextColor(rgb[0], rgb[1], rgb[2]);
+};
+
+function setAreaPositionEscalar(areaElement, margemTopo, margemEsquerda, fatorEscala, adicionalEixoY){
+  let x = (parseFloat((areaElement.style.left).slice(0,-2)))-margemEsquerda;
+  let y = (parseFloat((areaElement.style.top).slice(0,-2)))-margemTopo;
+  if (isNaN(x) && isNaN(y)){
+    return [0, adicionalEixoY];
+  } else if (isNaN(x)) {
+    return [0, (y / fatorEscala)+adicionalEixoY];
+  } else if (isNaN(y)) {
+    return [(x / fatorEscala), 0];
+  };
+  return [(x / fatorEscala), (y / fatorEscala)+adicionalEixoY];
+};
 
 function getTagsNaPlanilha(texto){
   var cabecalho = texto.slice(0, texto.indexOf("\n"));
@@ -143,7 +122,7 @@ function getTagsNaPlanilha(texto){
   return $tagsNaPlanilha;
 };
 
-function verificarTags(tagsNoCracha, tagsNoTexto){
+function verificarTags(){
   if ($tagsNoCracha.length == 0){
     tagsCount.innerText = "Não há tags em seu crachá";
     return false;
@@ -177,7 +156,7 @@ function criarParticipantes(){
       dados = dados.split("\t");
       if (dados.length === $tagsNaPlanilha.length){
         var participante = {};
-        for (let x = 0; dados[x]; x++){
+        for (let x = 0; x < dados.length; x++){
           participante[$tagsNaPlanilha[x]] = dados[x];
         };
         participantes.push(participante);
@@ -214,4 +193,4 @@ function converterCor(cor) {
     default:
       return "(0, 0, 0)";
   };
-}
+};
